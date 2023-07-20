d03593d8d0a505f5f13d97262d2404ec8dc7174a
Geovanni89 <vg.barbosa89@gmail.com>
2023-07-20 08:52:59 -0600

    input validation

--- src/Components/Cocktails/CocktailSearchForm.js
+++ src/Components/Cocktails/CocktailSearchForm.js
@@ -1,10 +1,21 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import {Button, TextField, Box, Grid, Paper} from "@mui/material";
 
 const CocktailSearchForm = ({isSearching, onSearchHandler}) => {
     const [cocktailName, setCocktailName] = useState("");
+    const [hasError, setHasError] = useState(false);
+
+    useEffect(() => {
+        if(cocktailName.length === 0) {
+            setHasError(true);
+            return;
+        }
+        setHasError(false);
+    }, [cocktailName]);
+
 
     const searchCocktailsHandler = () => {
+        if(cocktailName.length === 0) return;
         onSearchHandler(cocktailName);
     }
 
@@ -21,7 +32,13 @@ const CocktailSearchForm = ({isSearching, onSearchHandler}) => {
         <Paper sx={{margin: 12}} elevation={10}>
             <Grid container justifyContent="center" alignItems="center" spacing={3}>
                 <Grid item xl={8} md={8}>
-                    <TextField required id="cocktail-input" label="Cocktails" variant="outlined" value={cocktailName} onChange={onChangeHandler}/>
+                    <TextField 
+                        error={hasError} 
+                        required 
+                        id="cocktail-input" 
+                        label="Cocktails" 
+                        helperText="Write a cocktail's name."
+                        variant="outlined" value={cocktailName} onChange={onChangeHandler}/>
                 </Grid>
                 <Grid item xl={3} md={3}>
                     <Button variant="contained" onClick={searchCocktailsHandler} disabled={isSearching}>Search</Button>
